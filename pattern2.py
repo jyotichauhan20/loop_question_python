e0bf7a5ba16a16e1c6d593ddbdd811661a9aa916
jyotichauhan20 <72217749+jyotichauhan20@users.noreply.github.com>
2020-12-19 11:49:55 +0530

    Add files via upload

--- pattern2.py
+++ pattern2.py
@@ -1,7 +1,10 @@
 i=1
-while i<=100:
-    if i%2==0:
-        print(-i)
-    else:
-        print(i)
-    i=i+1        
+k=1
+while i<5:
+    j=0
+    while j<i:
+        print(k,end="")
+        j=j+1
+        k=k+1
+    print()
+    i=i+1
